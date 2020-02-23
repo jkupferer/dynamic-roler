a7c02c898bc7e94e6b1fb7625280beec8111258e
Johnathan Kupferer <jkupfere@redhat.com>
2020-02-23 10:21:26 -0600

    Add git source support

--- filter_plugins/dynamic_roler.py
+++ filter_plugins/dynamic_roler.py
@@ -10,23 +10,36 @@ ANSIBLE_METADATA = {
     'supported_by': 'community'
 }
 
+import hashlib
 import re
 from ansible.errors import AnsibleFilterError
 
-def dynamic_role_name(role):
-    if 'name' in role:
-        return role['name']
-    m = re.search(r'/(\w+)(\.git)?$', role['src'])
+def dynamic_roler_source_name(source):
+    if 'name' in source:
+        return source['name']
+    m = re.search(r'/(\w+)(\.git)?$', source['src'])
     if m:
         return m.group(1)
-    elif re.match('(\w+)\.(\w+)$', roule['src']):
-        return role['src']
-    raise AnsibleFilterError("Unable to determine role name from {}, name must be provided".format(role['src']))
+    elif re.match('(\w+)\.(\w+)$', source['src']):
+        return source['src']
+    raise AnsibleFilterError("Unable to determine source name from {}, name must be provided".format(source['src']))
+
+def dynamic_roler_git_source_repo_hash(source):
+    git = source['git']
+    name = dynamic_roler_source_name(source)
+    m = re.search(r'/(\w+)(\.git)?$', git['repo'])
+    prefix = m.group(1) + '-'
+    if 'version' in git:
+        prefix += git['version'] + '-'
+    return prefix + hashlib.sha256((
+        git['repo'] + ':' + git.get('version', '')
+    ).encode('utf-8')).hexdigest()
 
 # ---- Ansible filters ----
 class FilterModule(object):
 
     def filters(self):
         return {
-            'dynamic_role_name': dynamic_role_name
+            'dynamic_roler_git_source_repo_hash': dynamic_roler_git_source_repo_hash,
+            'dynamic_roler_source_name': dynamic_roler_source_name
         }
--- test_plugins/dynamic_roler.py
+++ test_plugins/dynamic_roler.py
@@ -8,24 +8,37 @@ import re
 def dynamic_roler_galaxy_source(role):
     return 'git' not in role
 
-def dynamic_roler_cache_disabled(role):
+def dynamic_roler_git_source(role):
+    return 'git' in role
+
+def dynamic_roler_cache_enabled(source):
     '''
-    Return whether cache is enabled for this role.
+    Return whether cache is enabled for this source.
     Cache may be enabled explicitly or otherwise detected from the version string.
     Versions that appear to use semantic versioning will be enabled for cache.
     '''
-    if 'cache' in role:
-        return not role['cache']
-    elif 'version' not in role:
-        return True
-    elif re.search(r'\d+\.\d+\.\d+', role['version']):
+    if 'cache' in source:
+        return bool(source['cache'])
+    elif 'git' in source:
+        source = source['git']
+    if 'version' not in source:
         return False
-    else:
+    elif re.search(r'\d+\.\d+\.\d+', source['version']):
         return True
+    else:
+        return False
+
+def dynamic_roler_cache_disabled(source):
+    '''
+    Return whether cache is disbled for this source.
+    '''
+    return not dynamic_roler_cache_enabled(source)
 
 class TestModule(object):
     def tests(self):
         return dict(
+            dynamic_roler_cache_disabled=dynamic_roler_cache_disabled,
+            dynamic_roler_cache_enabled=dynamic_roler_cache_enabled,
             dynamic_roler_galaxy_source=dynamic_roler_galaxy_source,
-            dynamic_roler_cache_disabled=dynamic_roler_cache_disabled
+            dynamic_roler_git_source=dynamic_roler_git_source
         )
